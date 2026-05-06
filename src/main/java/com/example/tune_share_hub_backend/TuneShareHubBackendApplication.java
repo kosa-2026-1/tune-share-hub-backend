c978fbbcf232647ded8c11d3a5530e9045b2bd09
jiye <jiye@jiyeui-MacBookPro.local>
2026-05-06 15:10:32 +0900

    feat: application에서 mapperscan 추가

--- src/main/java/com/example/tune_share_hub_backend/TuneShareHubBackendApplication.java
+++ src/main/java/com/example/tune_share_hub_backend/TuneShareHubBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.tune_share_hub_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.tune_share_hub_backend.mapper")
 public class TuneShareHubBackendApplication {
 
 	public static void main(String[] args) {
