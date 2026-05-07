d4edafbbbe6bbe9ca5cad583e2800efbdc3704d0
qqqkyj <123rkdduswn@naver.com>
2026-05-07 16:03:08 +0900

    Cors 설정

---
+++ src/main/java/com/example/tune_share_hub_backend/global/config/security/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.tune_share_hub_backend.global.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(// CORS 허용 대상 프론트 주소
+                        "http://localhost:3000"
+                )
+                .allowedMethods("*")// 허용할 HTTP 메서드
+                .allowedHeaders("*")// 요청 헤더 전체 허용
+                .exposedHeaders("Authorization")// 프론트에서 Authorization 응답 헤더 읽을 수 있게 함
+                .allowCredentials(true);// JWT + 쿠키 인증 시 필요
+    }
+}
