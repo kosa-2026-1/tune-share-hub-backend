9dcda52c93f3ef12a77b8c1945a9c0a4852e43f0
qqqkyj <123rkdduswn@naver.com>
2026-05-07 14:36:47 +0900

    Swagger Operation 추가

--- src/main/java/com/example/tune_share_hub_backend/controller/auth/AuthController.java
+++ src/main/java/com/example/tune_share_hub_backend/controller/auth/AuthController.java
@@ -3,7 +3,9 @@ package com.example.tune_share_hub_backend.controller.auth;
 import com.example.tune_share_hub_backend.dto.auth.LoginRequestDto;
 import com.example.tune_share_hub_backend.dto.auth.LoginResponseDto;
 import com.example.tune_share_hub_backend.dto.user.UserResponseDto;
+import com.example.tune_share_hub_backend.global.util.CookieUtil;
 import com.example.tune_share_hub_backend.service.auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -24,7 +26,9 @@ public class AuthController {
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
     private final AuthService authService;
+    private final CookieUtil cookieUtil;
 
+    @Operation(summary = "로그인", description = "사용자가 이메일과 비밀번호를 입력합니다.")
     @PostMapping("/login")
     public ResponseEntity<UserResponseDto> login(
             @Valid  @RequestBody LoginRequestDto request,
@@ -34,6 +38,18 @@ public class AuthController {
         log.info("Login attempt for email: {}", request.getEmail());
         return ResponseEntity.ok(loginResponse.getUserResponseDto());
     }
+
+//    @PostMapping("/reissue")
+//    public ResponseEntity<?> reissue(
+//            HttpServletRequest request,
+//            HttpServletResponse response
+//    ){
+//        String refresh = cookieUtil.getCookieValue(request, REFRESH_TOKEN_COOKIE_NAME);
+//        LoginRequestDto loginRequestDto = authService.reissue(refresh);
+//        setTokenResponse(response, loginRequestDto);
+//        log.info("Reissue attempt for email: {}", loginRequestDto.getEmail());
+//        return ResponseEntity.ok().build();
+//    }
 
     private void setTokenResponse(HttpServletResponse response, LoginResponseDto loginResponse){
         response.setHeader(AUTHORIZATION_HEADER, BEARER_PREFIX + loginResponse.getAccessToken());
