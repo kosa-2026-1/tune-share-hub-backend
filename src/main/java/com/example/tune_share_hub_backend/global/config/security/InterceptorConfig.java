cbaa31ba893b211b2e03f93f49749bb3ad3387d2
qqqkyj <123rkdduswn@naver.com>
2026-05-07 17:16:21 +0900

    로그인 사용자 ID 주입을 위한 ArgumentResolver 추가 및 토큰재발급(reissue 경로 패턴제거 추가)

--- src/main/java/com/example/tune_share_hub_backend/global/config/security/InterceptorConfig.java
+++ src/main/java/com/example/tune_share_hub_backend/global/config/security/InterceptorConfig.java
@@ -1,13 +1,17 @@
 package com.example.tune_share_hub_backend.global.config.security;
 
 import com.example.tune_share_hub_backend.global.interceptor.AccessTokenCheckInterceptor;
+import com.example.tune_share_hub_backend.global.interceptor.LoginUserIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     //JWT 검증 인터셉터
     private final AccessTokenCheckInterceptor atci;
+    private final LoginUserIdArgumentResolver luar;
 
     // 인터셉터를 Spring MVC 요청 처리 흐름에 등록
     @Override
@@ -23,6 +28,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/**")
                 .excludePathPatterns(
                         "/api/auth/login",
+                        "/api/auth/reissue",
 
                         // swagger
                         "/swagger-ui/**",
@@ -32,6 +38,12 @@ public class InterceptorConfig implements WebMvcConfigurer {
                         // 에러
                         "/error"
                 );
+    }
+
+    // 로그인한 사용자 ID를 컨트롤러 메서드 인자로 주입하는 ArgumentResolver 등록
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(luar);
     }
 
     @Bean
