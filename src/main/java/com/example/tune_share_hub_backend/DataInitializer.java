1b4d535c19a88c46b9c5f7ad7d3224c5401d1c80
josephuk77 <josephuk77@gmail.com>
2026-05-07 17:13:12 +0900

    fix: ci 테스트가 통과될 수 있게 더미값 추가

--- src/main/java/com/example/tune_share_hub_backend/DataInitializer.java
+++ src/main/java/com/example/tune_share_hub_backend/DataInitializer.java
@@ -6,6 +6,7 @@ import com.example.tune_share_hub_backend.entity.user.UserRoleType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
