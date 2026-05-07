cda1cd31a00d2af20a165a209b49a82434b64907
qqqkyj <123rkdduswn@naver.com>
2026-05-07 16:24:01 +0900

    JWT입력 스키마 추가

--- src/main/java/com/example/tune_share_hub_backend/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/tune_share_hub_backend/config/swagger/SwaggerConfig.java
@@ -1,19 +1,41 @@
 package com.example.tune_share_hub_backend.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Tune Share Hub API")
                         .description("Tune Share Hub 프로젝트 API 문서입니다.")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local Server")
+                ))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요 (Bearer 없이)")));
     }
 }
