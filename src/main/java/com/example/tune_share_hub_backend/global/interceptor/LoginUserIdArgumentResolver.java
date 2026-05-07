f98db8ac6d411c504de899cb7aec002b997e4a26
qqqkyj <123rkdduswn@naver.com>
2026-05-07 17:15:40 +0900

    로그인 사용자 ID 주입을 위한 ArgumentResolver 구현

---
+++ src/main/java/com/example/tune_share_hub_backend/global/interceptor/LoginUserIdArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.example.tune_share_hub_backend.global.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+
+@Component
+public class LoginUserIdArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUserId.class) && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (request != null) {
+            Object userIdAttr = request.getAttribute("userId");
+            if (userIdAttr instanceof Long userId) {
+                return userId;
+            }
+        }
+        throw new IllegalStateException("No userId found in request attributes");
+    }
+}
