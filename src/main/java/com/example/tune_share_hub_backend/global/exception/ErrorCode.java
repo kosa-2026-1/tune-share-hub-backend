c7899b59141badc7b9bf255425b5bcb2c5fa070b
qqqkyj <123rkdduswn@naver.com>
2026-05-08 14:02:35 +0900

    좋아요 에러코드 추가

--- src/main/java/com/example/tune_share_hub_backend/global/exception/ErrorCode.java
+++ src/main/java/com/example/tune_share_hub_backend/global/exception/ErrorCode.java
@@ -29,7 +29,11 @@ public enum ErrorCode {
     PLAYLIST_VISIBILITY_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 공개 여부를 변경할 수 없습니다."),
     INVALID_PUBLIC_YN(HttpStatus.BAD_REQUEST, "공개 여부는 Y 또는 N만 가능합니다."),
     INVALID_PLAYLIST_TITLE(HttpStatus.BAD_REQUEST, "플레이리스트 제목은 필수입니다."),
-    PLAYLIST_TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "플레이리스트에 노래를 찾을 수 없습니다." );
+    PLAYLIST_TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "플레이리스트에 노래를 찾을 수 없습니다." ),
+
+    //Like
+    LIKE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 좋아요가 존재합니다."),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
