35d92ea0a28133a3596c13e43d3a3f3841db577f
josephuk77 <josephuk77@gmail.com>
2026-05-08 10:44:14 +0900

    fix: ci실패로 test코드 수정 (#13)

--- src/test/java/com/example/tune_share_hub_backend/TuneShareHubBackendApplicationTests.java
+++ src/test/java/com/example/tune_share_hub_backend/TuneShareHubBackendApplicationTests.java
@@ -2,24 +2,28 @@ package com.example.tune_share_hub_backend;
 
 import com.example.tune_share_hub_backend.dao.playlist.PlaylistMapperDao;
 import com.example.tune_share_hub_backend.dao.playlist.PlaylistTrackDao;
+import com.example.tune_share_hub_backend.dao.refresh.RefreshTokenDao;
 import com.example.tune_share_hub_backend.dao.user.UserDao;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TuneShareHubBackendApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	private UserDao userDao;
 
-	@MockBean
+	@MockitoBean
 	private PlaylistMapperDao playlistMapperDao;
 
-	@MockBean
+	@MockitoBean
 	private PlaylistTrackDao playlistTrackDao;
+
+	@MockitoBean
+	private RefreshTokenDao refreshTokenDao;
 
 	@Test
 	void contextLoads() {
