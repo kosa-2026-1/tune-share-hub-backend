b9ec73c624b61e567807c57be44cb3abd04cacbd
qqqkyj <123rkdduswn@naver.com>
2026-05-09 10:34:06 +0900

    파일이름 변경 및 Dao @Param추가

--- src/main/java/com/example/tune_share_hub_backend/dao/like/LikeDao.java
+++ src/main/java/com/example/tune_share_hub_backend/dao/like/LikeDao.java
@@ -3,6 +3,7 @@ package com.example.tune_share_hub_backend.dao.like;
 import com.example.tune_share_hub_backend.entity.Playlist;
 import com.example.tune_share_hub_backend.entity.like.Like;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,8 +12,7 @@ public interface LikeDao {
     int incrementLikeCount(Long playlistId);
     int decrementLikeCount(Long playlistId);
     List<Playlist> getLikedPlaylistsByUserId(Long userId);
-    Like getLikeByUserIdAndPlaylistId(Long playlistId, Long userId);
-    int insertLike(Long playlistId, Long userId, String status);
-    int updateLikeStatus(Long playlistId,Long userId, String status);
+    Like getLikeByUserIdAndPlaylistId(@Param("playlistId") Long playlistId, @Param("userId") Long userId);
+    int insertLike(@Param("playlistId") Long playlistId, @Param("userId") Long userId, @Param("status") String status);
+    int updateLikeStatus(@Param("playlistId") Long playlistId, @Param("userId") Long userId, @Param("status") String status);
 }
-
--- src/main/java/com/example/tune_share_hub_backend/dao/refresh/RefreshTokenDao.java
+++ src/main/java/com/example/tune_share_hub_backend/dao/refresh/RefreshTokenDao.java
@@ -2,11 +2,12 @@ package com.example.tune_share_hub_backend.dao.refresh;
 
 import com.example.tune_share_hub_backend.entity.refresh.Refresh;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface RefreshTokenDao {
     public void insert(Refresh refresh);
-    public int deleteByUserIdAndTokenValue(Long userId, String refreshToken);
-    public int existsRefresh(String refreshToken, Long userId);
-    public int revokeTokensByUserIdAndTokenValue(Long userId, String refreshToken);
+    public int deleteByUserIdAndTokenValue(@Param("userId") Long userId, @Param("refreshToken") String refreshToken);
+    public int existsRefresh(@Param("refreshToken") String refreshToken, @Param("userId") Long userId);
+    public int revokeTokensByUserIdAndTokenValue(@Param("userId") Long userId, @Param("refreshToken") String refreshToken);
 }
