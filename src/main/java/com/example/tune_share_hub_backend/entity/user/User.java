b67cf1a1c3b22e52461190cc06dda4c177120719
qqqkyj <123rkdduswn@naver.com>
2026-05-09 19:49:05 +0900

    @setter제거

--- src/main/java/com/example/tune_share_hub_backend/entity/user/User.java
+++ src/main/java/com/example/tune_share_hub_backend/entity/user/User.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
