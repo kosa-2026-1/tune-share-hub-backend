90488589420649e5d9bdc78bae1308a285912eb0
qqqkyj <123rkdduswn@naver.com>
2026-05-12 12:50:10 +0900

    Playlist -> PlaylistResponseDto 수정

--- src/main/java/com/example/tune_share_hub_backend/dao/like/LikeDao.java
+++ src/main/java/com/example/tune_share_hub_backend/dao/like/LikeDao.java
@@ -1,6 +1,6 @@
 package com.example.tune_share_hub_backend.dao.like;
 
-import com.example.tune_share_hub_backend.entity.Playlist;
+import com.example.tune_share_hub_backend.dto.playlist.PlaylistResponseDto;
 import com.example.tune_share_hub_backend.entity.like.Like;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,7 +11,7 @@ import java.util.List;
 public interface LikeDao {
     int incrementLikeCount(Long playlistId);
     int decrementLikeCount(Long playlistId);
-    List<Playlist> getLikedPlaylistsByUserId(Long userId);
+    List<PlaylistResponseDto> getLikedPlaylistsByUserId(Long userId);
     Like getLikeByUserIdAndPlaylistId(@Param("playlistId") Long playlistId, @Param("userId") Long userId);
     int insertLike(@Param("playlistId") Long playlistId, @Param("userId") Long userId, @Param("status") String status);
     int updateLikeStatus(@Param("playlistId") Long playlistId, @Param("userId") Long userId, @Param("status") String status);
