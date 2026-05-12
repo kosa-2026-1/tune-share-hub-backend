30bdde895ba31dd7b94dccef06e6fdcf866415d3
josephuk77 <josephuk77@gmail.com>
2026-05-12 15:41:47 +0900

    refactor: 변환 로직을 convert 계층으로 정리

--- src/main/java/com/example/tune_share_hub_backend/dto/user/UserResponseDto.java
+++ src/main/java/com/example/tune_share_hub_backend/dto/user/UserResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.tune_share_hub_backend.dto.user;
 
-import com.example.tune_share_hub_backend.entity.user.User;
 import com.example.tune_share_hub_backend.entity.user.UserRoleType;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,13 +11,4 @@ public class UserResponseDto {
     private String email;
     private String nickname;
     private UserRoleType role;
-
-    public static UserResponseDto from(User user){
-        return UserResponseDto.builder()
-                .userId(user.getUserId())
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-                .role(user.getRole())
-                .build();
-    }
 }
